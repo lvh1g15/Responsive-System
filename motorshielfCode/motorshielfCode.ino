2e0b73ac9ba3b2aef937055634ed88f6391d6f63
lvh1g15 <landon2008@hotmail.com>
2017-04-12 12:00:51 +0100

    working stepper on motorshield

--- motorshielfCode/motorshielfCode.ino
+++ motorshielfCode/motorshielfCode.ino
@@ -1,15 +1,43 @@
 #include <Wire.h>
 #include <Adafruit_MotorShield.h>
+//#include "utility/Adafruit_MS_PWMServoDriver.h"
 
-Adafruit_MotorShield AFMS = Adafruit_MotorShield();
-Adafruit_StepperMotor myStepper = AFMS.getStepper(200,2)
+// Create the motor shield object with the default I2C address
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
+// Or, create it with a different I2C address (say for stacking)
+// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
+
+// Connect a stepper motor with 200 steps per revolution (1.8 degree)
+// to motor port #2 (M3 and M4)
+Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+  Serial.println("Stepper test!");
 
+  AFMS.begin();  // create with the default frequency 1.6KHz
+  //AFMS.begin(1000);  // OR with a different frequency, say 1KHz
+  
+  myMotor->setSpeed(60);  // 10 rpm   
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  myStepper.setSpeed(60);
+  Serial.println("Single coil steps");
+  myMotor->step(1000, FORWARD, DOUBLE); 
+  delay(100);
+  myMotor->step(1000, BACKWARD, DOUBLE); 
+//
+//  Serial.println("Double coil steps");
+//  myMotor->step(100, FORWARD, DOUBLE); 
+//  myMotor->step(100, BACKWARD, DOUBLE);
+//  
+//  Serial.println("Interleave coil steps");
+//  myMotor->step(100, FORWARD, INTERLEAVE); 
+//  myMotor->step(100, BACKWARD, INTERLEAVE); 
+//  
+//  Serial.println("Microstep steps");
+//  myMotor->step(50, FORWARD, MICROSTEP); 
+//  myMotor->step(50, BACKWARD, MICROSTEP);
 }
