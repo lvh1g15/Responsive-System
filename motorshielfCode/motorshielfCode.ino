79ddc4b3f31c22de56263df9b7f618726b79a590
lvh1g15 <landon2008@hotmail.com>
2017-04-12 19:43:08 +0100

    solved issues

--- motorshielfCode/motorshielfCode.ino
+++ motorshielfCode/motorshielfCode.ino
@@ -7,13 +7,13 @@ Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_StepperMotor *myMotor = AFMS.getStepper(200, 2);
 Adafruit_StepperMotor *myMotor2 = AFMS.getStepper(32, 1);
 
-
+int counter = 0;
 const int pingPin = 3;
 long cm;
 long db;
 long finalUltraSoundValue = 0;
 int sensorPin = A0;
-long arraySensorValues[1] = {};
+long arraySensorValues[] = {cm, db};
 
 void setup() {
   Serial.begin(9600);           // set up Serial library at 9600 bps
@@ -45,9 +45,10 @@ long microsecondsToCentimeters(long microseconds) {
 }
 
 void loop() {
-runmotorAndReadSensor(100);
-delay(1000);
-
+  for(int m=0; m<sizeof(arraySensorValues);m++) {
+    runmotorAndReaddata(100, m);
+    delay(100);
+  }
 }
 
 void microphone() {
@@ -55,34 +56,60 @@ void microphone() {
   arraySensorValues[1] = db;
 }
 
-void runmotorAndReadSensor(int input) { 
+void runmotorAndReaddata(int input, int m) { 
   int i = 0;
-  int m = 0;
-    for(int i; i < input; i++) {
       do {
-         myMotor2->step(input, FORWARD, DOUBLE);
+         myMotor2->step(100, FORWARD, DOUBLE);
          ultraSoundResponse();
+         delay(100);
          microphone();
+         delay(100);
       }
-      while (i >= input);
+      while (counter >= input);
+        counter += 1;
         myMotor2->release();
+        delay(100);
         mappingStepperData(arraySensorValues[m]);
-     } 
 }
+//
+//void runmotorAndReadAudio(int input) { 
+//
+//    do {
+//       myMotor2->step(100, FORWARD, DOUBLE);
+//       microphone();
+//       delay(100);
+//       ultraSoundResponse();
+//       delay(100);
+//    }
+//    while (counter >= input);
+//      counter += 1;
+//      myMotor2->release();
+//      delay(100);
+//      mappingStepperData(arraySensorValues[1]);
+//}
 
-void mappingStepperData(int sensorValue) {
+void mappingStepperData(long sensorValue) {
   // ultrasound comes in cm but we turn into how far stepper moves between 0 and 2000
   if(sensorValue > 315) {
-    int mappedAudio = map(sensorValue, 450, 800, 0, 2000);
+    long mappedAudio = map(sensorValue, 450, 800, 0, 1300);
     myMotor->step(mappedAudio, FORWARD, DOUBLE);
+    myMotor->release();
+    Serial.print(" AUDIO...");
+    Serial.print(" ULTRA: ");
+    Serial.print(arraySensorValues[0]);
+    Serial.print(" AUDIO: ");
+    Serial.print(arraySensorValues[1]);
+    Serial.println("");
   }
   else {
-    int mappedStepper = map(sensorValue, 0, 315, 0, 2000);
+    long mappedStepper = map(sensorValue, 0, 315, 0, 1300);
     myMotor->step(mappedStepper, FORWARD, DOUBLE);
-    Serial.print(" cm: ");
-    Serial.print(cm);
-    Serial.print(", db: ");
-    Serial.print(db);
+    myMotor->release();
+    Serial.print(" ULTRASOUND... ");
+    Serial.print(" ULTRA: ");
+    Serial.print(arraySensorValues[0]);
+    Serial.print(" AUDIO: ");
+    Serial.print(arraySensorValues[1]);
     Serial.println("");
   }
 }
